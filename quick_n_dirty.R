bbc9f8efb36f9f36abef3d35fcb67780f2bac4d9
kymosh <kyliecmosher@gmail.com>
2025-07-16 14:39:34 -0600

    checked to see if KNP fire has SWE data from ASO

--- quick_n_dirty.R
+++ quick_n_dirty.R
@@ -1,5 +1,5 @@
 # Load packages
-packages <- c( 'here', 'dplyr', 'stringr', 'terra', 'tibble', 'ggplot2')
+packages <- c( 'here', 'dplyr', 'stringr', 'terra', 'tibble', 'ggplot2', 'sf')
 install.packages(setdiff(packages, rownames(installed.packages())))
 lapply(packages, library, character.only = TRUE)
 
@@ -8,3 +8,16 @@ res(tif)
 
 #test change
 # test again
+
+
+### Check to see if KNP fire has SWE data from ASO
+knp <- st_read(here('data', 'raw', 'fire_info', 'shp', 'knp_fire_perimeter.shp'))
+kaweah <- rast(here('data', 'raw', 'ASO', 'tif', 'ASO_Kaweah_2024Apr03_swe_50m.tif'))
+
+crs(knp, describe = T)$code
+crs(kaweah, describe = T)$code
+
+knp.32611 <- st_transform(knp, crs(kaweah))
+
+plot(kaweah)
+plot(st_geometry(knp.32611), add = TRUE, border = 'red')
