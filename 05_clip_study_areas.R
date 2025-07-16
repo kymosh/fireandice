e8a74758151fb071fa2a9cf42b449a03240211ec
kymosh <kyliecmosher@gmail.com>
2025-07-16 15:45:26 -0600

    update to clip ASO files to only >5000ft

--- 05_clip_study_areas.R
+++ 05_clip_study_areas.R
@@ -14,7 +14,7 @@
 # plot(st_geometry(creek.extent), col = adjustcolor('green', alpha.f = 0.5), main = 'Creek Study Extent')
 # plot(st_geometry(castle.extent), col = adjustcolor('purple', alpha.f = 0.5), main = 'Castle Study Extent')
 # 
-# # Load ASO SWE raster
+# Load ASO SWE raster
 # tif <- rast(here('data', 'raw', 'ASO', 'tif', 'ASO_SanJoaquin_2023Jan21-24_swe_50m.tif'))
 # 
 # # Check coordinate reference systems
@@ -36,9 +36,27 @@ lapply(packages, library, character.only = TRUE)
 castle.extent <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_castle_32611.shp'))
 creek.extent  <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_creek_32611.shp'))
 
+# Read DEMs
+dem.castle <- rast(here('data', 'processed', 'processed', 'tif', 'dem_castle_32611.tif'))
+dem.creek <- rast(here('data', 'processed', 'processed', 'tif', 'dem_creek_32611.tif'))
 
+# Load sample ASO SWE raster
+tif <- rast(here('data', 'raw', 'ASO', 'tif', 'ASO_SanJoaquin_2023Jan21-24_swe_50m.tif'))
 
+# make sure tifs match extents and dem CRS
+crs(dem.castle, describe = T)$code == crs(tif, describe = T)$code
+crs(tif, describe = T)$code == crs(castle.extent, describe = T)$code
 
+######################
+# create mask to limit elevations to >5000ft (1524m)
+creek.mask <- dem.creek > 1524
+castle.mask <- dem.castle > 1524
+
+# use mask to filter DEM
+dem.creek.5000 <- mask(dem.creek, creek.mask, maskvalue = 0)
+dem.castle.5000 <- mask(dem.castle, castle.mask, maskvalue = 0)
+plot(dem.castle.5000)
+plot(dem.creek.5000)
 
 ######################
 # ASO tifs
@@ -62,16 +80,24 @@ kaweah.tifs <- tif.files[str_detect(tif.files, 'Kaweah')]
 castle.tifs <- c(kern.tifs, kaweah.tifs)
 
 # Helper function to clip and save rasters
-clip.and.save <- function(file.path, extent.sf, out.dir.tif) {
-  # Load raster
+clip.and.save <- function(file.path, extent.sf, out.dir.tif, dem.masked) {
+  # Load SWE raster
   tif <- rast(file.path)
   
-  # Reproject extent to match raster
+  # Reproject extent to match SWE raster
   extent.sf <- st_transform(extent.sf, crs(tif))
   
   # Clip to bounding box and mask to shape
   tif.crop <- crop(tif, vect(extent.sf))
   tif.mask <- mask(tif.crop, vect(extent.sf))
+  
+  # Resample DEM to match SWE raster
+  if (!compareGeom(tif.mask, dem.masked, stopOnError = FALSE)) {
+    dem.masked <- resample(dem.masked, tif.mask, method = 'near')
+  }
+  
+  # Apply elevation mask for >5000ft (1524m)
+  tif.elev.masked <- mask(tif.mask, dem.masked, maskvalue = NA)
   
   # Create output name
   base.name <- tools::file_path_sans_ext(basename(file.path))
@@ -79,24 +105,26 @@ clip.and.save <- function(file.path, extent.sf, out.dir.tif) {
   out.path <- file.path(out.dir.tif, out.name)
   
   # Save
-  writeRaster(tif.mask, out.path, overwrite = TRUE)
+  writeRaster(tif.elev.masked, out.path, overwrite = TRUE)
 }
 
 # Clip SanJoaquin files to creek.extent
 for (f in creek.tifs) {
-  clip.and.save(f, creek.extent, out.dir.tif)
+  clip.and.save(f, creek.extent, out.dir.tif, dem.creek.5000)
 }
 
 # Clip Kern and Kaweah files to castle.extent
 for (f in castle.tifs) {
-  clip.and.save(f, castle.extent, out.dir.tif)
+  clip.and.save(f, castle.extent, out.dir.tif, dem.castle.5000)
 }
 
 
+# check example
+tifx <- rast(here('data', 'processed', 'processed', 'tif', 'ASO_SanJoaquin_Mosaic_2021Mar31-Apr1_swe_50m_clipped.tif'))
+tify <- rast(here('data', 'processed', 'processed', 'tif', 'ASO_Kaweah_2024Feb11_swe_50m_clipped.tif'))
 
-
-
-
+plot(tifx)
+plot(tify)
 ####################
 # img topography files
 
