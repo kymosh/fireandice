bf8fb22b7fb12f2af470c0b46e77f2d95c9daec4
kymosh <kyliecmosher@gmail.com>
2025-08-27 16:38:03 -0600

    geting shp files ready to pull into GEE to get fire data for JFSP

---
+++ data/raw/fire_info/shp/MTBS_within_ASO_and_QL1_README.txt
@@ -0,0 +1,3 @@
+Created by Kylie Mosher on 8/27/25 
+downloaded from GEE
+MTBS
--- jfsp_data_exploration.R
+++ jfsp_data_exploration.R
@@ -12,9 +12,13 @@ us <- st_transform(us, crs = 5070)
 # western states
 western.states <- c('WA', 'OR', 'CA', 'NV', 'ID', 'MT', 'WY', 'UT', 'CO', 'AZ', 'NM')
 us.west <- us[us$postal %in% western.states, ]
-# 
-# 
-# 
+# get rid of unnecessary columns
+us.west <- us.west %>%
+  select(name)
+
+
+
+
 # # Load the GeoPackage that contains lidar data
 # wesm <- st_read(here('data', 'raw', 'other', 'WESM.gpkg'))
 # 
@@ -46,23 +50,43 @@ us.west <- us[us$postal %in% western.states, ]
 # # calculate overlap
 # aso.ql1 <- sf::st_intersection(aso.extents.sf, wesm.ql1.west)
 
-aso.ql1 <- aso.ql1 %>% 
-  select(ql, collect_start, collect_end, workunit)
+
+aso.ql1 <- st_read(here('data', 'processed', 'processed', 'gpkg', 'aso_ql1_overlap.gpkg'))
+aso.ql1.simple <- aso.ql1 %>% 
+  select(ql, collect_start, collect_end, workunit) %>%
+  st_simplify(dTolerance = 50) %>%
+  st_make_valid() %>%
+  .[!st_is_empty(.), ] %>%                             # remove empty geometries
+  st_cast("MULTIPOLYGON") 
 
 
-st_write(aso.ql1, here('data', 'processed', 'processed', 'shp', 'aso_ql1_overlap.shp'), delete_layer = T)
-# # st_write(aso.ql1, here('data', 'processed', 'processed', 'gpkg', 'aso_ql1_overlap.gpkg'), delete_layer = T)
+st_write(aso.ql1.simple, here('data', 'processed', 'processed', 'shp', 'aso_ql1'), delete_layer = T)
 
 
 ggplot() +
   geom_sf(data = us.west, fill = 'grey95', color = 'grey70') +
   #geom_sf(data = wesm.ql1.west, fill = 'palegreen3', color = NA) +
   #geom_sf(data = aso.extents.sf, fill = '#2c7fb8', color = NA, alpha = 0.5) +
-  geom_sf(data = aso.ql1, fill = 'purple', color = NA, alpha = 0.6) +
+  geom_sf(data = aso.ql1.simple, fill = 'purple', color = NA, alpha = 0.6) +
   coord_sf(expand = FALSE) +
   theme_minimal() + 
   labs(title = 'ASO Basin Coverage & QL1 LiDAR Extents',
        subtitle = 'Blue = ASO data, Green = QL1 Lidar, \nPurple = ASO & QL1 overlap')
 
-wesm.ql1.west  <- readRDS(here('data', 'processed', 'processed', 'rds', 'wesm_ql1_west.rds'))
-aso.ql1 <- st_read(here('data', 'processed', 'processed', 'gpkg', 'aso_ql1_overlap.gpkg'))
+
+###############################
+# Used GEE to get MTBS fire data for the aso.ql1.simple shapefile
+
+fires <- st_read(here('data', 'raw', 'fire_info', 'shp', 'MTBS_within_ASO_and_QL1.shp'))
+
+ggplot
+
+ggplot() +
+  geom_sf(data = us.west, fill = 'grey95', color = 'grey70') +
+  #geom_sf(data = wesm.ql1.west, fill = 'palegreen3', color = NA) +
+  #geom_sf(data = aso.extents.sf, fill = '#2c7fb8', color = NA, alpha = 0.5) +
+  geom_sf(data = aso.ql1.simple, fill = 'purple', color = NA, alpha = 0.6) +
+  coord_sf(expand = FALSE) +
+  theme_minimal() + 
+  labs(title = 'ASO Basin Coverage & QL1 LiDAR Extents',
+       subtitle = 'Blue = ASO data, Green = QL1 Lidar, \nPurple = ASO & QL1 overlap')
