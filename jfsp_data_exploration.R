c6be749ab0f37a35846e3ce14ad22621cc0e8d7e
kymosh <kyliecmosher@gmail.com>
2025-09-02 12:18:00 -0600

    x

--- jfsp_data_exploration.R
+++ jfsp_data_exploration.R
@@ -51,8 +51,8 @@ us.west <- us.west %>%
 # aso.ql1 <- sf::st_intersection(aso.extents.sf, wesm.ql1.west)
 
 
-aso.ql1 <- st_read(here('data', 'processed', 'processed', 'gpkg', 'aso_ql1_overlap.gpkg'))
-aso.ql1.simple <- aso.ql1 %>% 
+#aso.ql1 <- st_read(here('data', 'processed', 'processed', 'gpkg', 'aso_ql1_overlap.gpkg'))
+#aso.ql1.simple <- aso.ql1 %>% 
   select(ql, collect_start, collect_end, workunit) %>%
   st_simplify(dTolerance = 50) %>%
   st_make_valid() %>%
@@ -61,7 +61,7 @@ aso.ql1.simple <- aso.ql1 %>%
 
 
 # st_write(aso.ql1.simple, here('data', 'processed', 'processed', 'shp', 'aso_ql1'), delete_layer = T)
-
+aso.ql1 <- st_read(here('data', 'processed', 'processed', 'shp', 'aso_ql1.shp'))
 
 ggplot() +
   geom_sf(data = us.west, fill = 'grey95', color = 'grey70') +
@@ -81,21 +81,46 @@ fires <- st_read(here('data', 'raw', 'fire_info', 'geojson', 'MTBS_within_ASO_an
 
 fires.after2017 <- fires %>%
   mutate(
-    ig_date = as.Date(as.POSIXct(Ig_Date/1000, origin = '1970-01-01', tz = 'UTC')),
+    ig_date = as.Date(as.POSIXct(Ig_Date/1000, origin = '1970-01-01', tz = 'UTC')), # change date to normal format
     ig_year = as.integer(format(ig_date, '%Y'))
   ) %>%
-  filter(ig_year > 2017) %>%
-  select(Incid_Name, Incid_Type, ig_date, ql, cllct_s, cllct_n, workunt, geometry)
+  filter(ig_year > 2017) %>% # only years 2017 and on
+  select(Incid_Name, Incid_Type, ig_date) %>% # cut unnecessary columns
 
+  library(sf)
+library(dplyr)
+
+fires.after2017.clean <- fires.after2017 %>%
+  # Expand collections into their pieces
+  st_cast("GEOMETRYCOLLECTION", warn = FALSE) %>%
+  # Extract only polygons from those
+  st_collection_extract("POLYGON") %>%
+  # Drop any empties
+  filter(!st_is_empty(geometry)) %>%
+  # Standardize to multipolygon
+  st_cast("MULTIPOLYGON") %>%
+  # Repair any invalid geometry
+  st_make_valid()
+
+
+st_write(fires.after2017.clean,
+         here("data", "processed", "processed", "shp", "fires_after2017_overlap.shp"),
+         delete_layer = TRUE)
+
+
+st_write(fires.after2017, here('data', 'processed', 'processed', 'shp', 'fires_after2017_overlap.shp'))
+
+crs(fires.after2017, describe = T)$code
+crs(aso.ql1, describe = T)$code
 
 ggplot() +
   geom_sf(data = us.west, fill = 'grey95', color = 'grey70') +
   #geom_sf(data = wesm.ql1.west, fill = 'palegreen3', color = NA) +
   #geom_sf(data = aso.extents.sf, fill = '#2c7fb8', color = NA, alpha = 0.5) +
-  geom_sf(data = aso.ql1.simple, fill = 'purple', color = NA, alpha = 0.6) +
+  geom_sf(data = aso.ql1, fill = 'purple', color = NA, alpha = 0.6) +
+  geom_sf(data = fires.after2017, fill = 'red', color = NA, alpha = 0.6) +
   coord_sf(expand = FALSE) +
   theme_minimal() + 
-  labs(title = 'ASO Basin Coverage & QL1 LiDAR Extents',
-       subtitle = 'Blue = ASO data, Green = QL1 Lidar, \nPurple = ASO & QL1 overlap')
+  labs(title = 'ASO & Lidar with Fires Overlap',
+       subtitle = '\nPurple = ASO & QL1 overlap, Red = Fires')
 
-#### test for git
