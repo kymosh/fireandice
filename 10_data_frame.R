2e0d3fba160623047746da8e1f9f6e6c95305d78
kymosh <kyliecmosher@gmail.com>
2025-10-01 16:11:59 -0600

    created preliminary code to create dataframes, though currently no forest structure metrics are included. Did not save outputs.

--- 10_data_frame.R
+++ 10_data_frame.R
@@ -1,143 +1,95 @@
-# Load packages
-packages <- c( 'here', 'dplyr', 'stringr', 'terra', 'tibble', 'ggplot2')
+packages <- c('terra', 'dplyr', 'here')
 install.packages(setdiff(packages, rownames(installed.packages())))
-lapply(packages, library, character.only = TRUE)
+lapply(packages, library, character.only = T)
 
-# ASO
-# Directory with input TIFs
+
+#### this is preliminary code to create dfs out of all my variables. as of 10/1 it works, but there is currently no forest metrics included. 
+
+#### SDD ####
+
 tif.dir <- here('data', 'processed', 'processed', 'tif')
+sdd.files <- list.files(here(tif.dir, '500m'), full.names = T)
 
-# List files with matching keywords
-tif.files <- list.files(tif.dir, pattern = '\\.tif$', full.names = TRUE)
+sdd.stack <- rast(sdd.files)
 
-# Filter files by basin
-creek.tifs   <- tif.files[str_detect(tif.files, 'SanJoaquin.*clipped')]
+# simplify names 
+clean.names <- sdd.files %>%
+  basename() %>%
+  gsub('^creek_', '', .) %>%
+  gsub('(_(\\d+m|32611))?_1524_2674', '', ., perl = TRUE) %>% 
+  gsub('\\.tif$', '', .) %>%
+  gsub('_', '.', .) %>%
+  gsub('terraclimate.', '', .) %>%
+  gsub('topo.', '', .) %>%
+  gsub('^\\.', '', .)
+  
+clean.names
 
-file_info <- tibble(fname = basename(creek.tifs)) %>%
-  mutate(
-    fire       = 'creek',
-    year       = str_extract(fname, '\\d{4}'),
-    month      = str_extract(fname, '(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'),
-    day        = str_extract(fname, '(?<=\\d{4}[A-Za-z]{3})\\d{1,2}'),  # 1- or 2-digit day
-    date       = as.Date(str_c(year, month, str_pad(day, 2, pad = '0'), sep = '-'), format = '%Y-%b-%d'),
-    variable   = str_extract(fname, '(?<=_)swe|depth|melt(?=_)'),  # tweak if needed
-    resolution = str_extract(fname, '\\d{2,}m')
-  )
+# extract layer names from stacked rasters (the terraclimate files and add to layer names)
+raster.list <- lapply(sdd.files, rast)
+layer.names <- mapply(function(r, n) {
+  rnames <- names(r)
+  if (length(rnames) == 1) {
+    n  # single-layer raster just gets cleaned file name
+  } else {
+    paste0(n, ".", rnames)  # multi-layer raster: base name + original band names
+  }
+}, raster.list, clean.names, SIMPLIFY = TRUE) %>% unlist()
 
-# remove report tifs/superswe tifs
-file_info <- file_info[-c(1, 2, 18), ]
+layer.names
+
+sdd.stack <- rast(raster.list)
+names(sdd.stack) <- layer.names
+
+# convert to dataframe
+sdd.df <- as.data.frame(sdd.stack, xy = T, na.rm = T)
+
+head(sdd.df)
 
 
 
-### NOTE code below was replicated and refined in another file, creek_terrain_dataframe.r
 
-##################### Creek Fire terrain variable stack
-# # create list of all creek variable files
-# # variable base names
-# var.names <- c('tpi_130', 'tpi_510', 'tpi_2010', 'slope', 'hli', 'dem')
-# # build full paths
-# creek.variables <- here('data', 'processed', 'processed', 'tif', paste0(var.names, '_creek_32611.tif'))
-# 
-# 
-# # check CRS of all variables
-# # unique(sapply(creek.variables, function(p) crs(rast(p), describe = TRUE)$code))
-# # all 32611, g2g
-# 
 
-# ###### check resolution of all variables
-# #unique(sapply(creek.variables, function(p) paste(res(rast(p)), collapse = 'x')))
-# 
-# # choose smallest res (10m) raster as reference raster
-# ref <- rast(creek.variables[which.min(sapply(creek.variables, function(p) prod(res(rast(p)))))])
-# # resample rasters to match
-# creek.variables.resampled <- lapply(creek.variables, function(p) {
-#   r <- rast(p)
-#   resample(r, ref, method = 'bilinear') 
-# })
-# 
-# # unique(sapply(creek.variables.resampled, function(p) paste(res(rast(p)), collapse = 'x')))
-# # # all 10m, g2g
-# # 
-# # 
-# # ###### check extents for all variables
-# names(creek.variables.resampled) <- var.names
-# for (name in names(creek.variables.resampled)) {
-#   r <- creek.variables.resampled[[name]]
-#   cat(name, '\n')
-#   print(ext(r))
-#   cat('\n')
-# }
-# # # g2g
-# 
-# creek.stack <- rast(creek.variables.resampled)
-# creek.terrain.df <- as.data.frame(creek.stack, xy = TRUE, na.rm = TRUE)
-# 
-# # check summary
-# #summary(creek.terrain.df)
-# .
-# ######## data visualization
-# # ggplot(creek.terrain.df, aes(x = tpi_130)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of TPI (130)', x = 'x', y = 'Count')
-# # # normal distribution
-# # 
-# # ggplot(creek.terrain.df, aes(x = tpi_510)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of TPI (510)', x = 'x', y = 'Count')
-# # # normal distribution
-# # 
-# # ggplot(creek.terrain.df, aes(x = tpi_2010)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of TPI (2010)', x = 'x', y = 'Count')
-# # # normal distribution
-# # 
-# # ggplot(creek.terrain.df, aes(x = slope)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of Slope', x = 'x', y = 'Count')
-# # ## skewed right
-# # 
-# # ggplot(creek.terrain.df, aes(x = hli)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of HLI (Heat Load Index)', x = 'x', y = 'Count')
-# # # normal distribution
-# # 
-# # ggplot(creek.terrain.df, aes(x = elevation)) +
-# #   geom_histogram(bins = 50, fill = 'skyblue', color = 'black') +
-# #   theme_minimal() +
-# #   labs(title = 'Distribution of Elevation', x = 'x', y = 'Count')
-# ## bimodal-ish
-# 
-# 
-# 
-# 
-# 
-# 
-# ######### re-scale variables to 0-1
-# # copy for safety
-# creek.terrain.scaled.df <- creek.terrain.df
-# 
-# # Get variable names to rescale (excluding x, y, and hli)- hli is already rescaled
-# vars.to.scale <- setdiff(names(creek.terrain.scaled.df), c('x', 'y', 'hli'))
-# 
-# # re-scale selected variables
-# creek.terrain.scaled.df[vars.to.scale] <- lapply(creek.terrain.scaled.df[vars.to.scale], function(x) {
-#   (x - min(x)) / (max(x) - min(x))
-# })
-# 
-# # check for NAs
-# #sum(is.na(creek.terrain.scaled.df))
-# 
-# # summary stats
-# #summary(creek.terrain.scaled.df)
-# 
-# ######### save dataframe
-# 
-# # save as CSV
-# write.csv(creek.terrain.scaled.df, file = here::here('data', 'processed',  'dataframes', 'creek_terrain_scaled.csv'), row.names = FALSE)
-# # save as RDS
-# saveRDS(creek.terrain.scaled.df, file = here::here('data', 'processed', 'dataframes', 'creek_terrain_scaled.rds'))
+#### SWE ####
+
+
+swe.files <- list.files(here(tif.dir, '50m'), full.names = T)
+
+swe.stack <- rast(swe.files)
+
+# simplify names 
+clean.names <- swe.files %>%
+  basename() %>%
+  gsub('^creek_', '', .) %>%
+  gsub('(_(\\d+m|32611))?_1524_2674', '', ., perl = TRUE) %>% 
+  gsub('\\.tif$', '', .) %>%
+  gsub('_', '.', .) %>%
+  gsub('terraclimate.', '', .) %>%
+  gsub('topo.', '', .) %>%
+  gsub('^\\.', '', .) %>%
+  gsub('^ASO.SanJoaquin.', '', .)
+
+
+clean.names
+
+# extract layer names from stacked rasters (the terraclimate files and add to layer names)
+raster.list <- lapply(swe.files, rast)
+layer.names <- mapply(function(r, n) {
+  rnames <- names(r)
+  if (length(rnames) == 1) {
+    n  # single-layer raster just gets cleaned file name
+  } else {
+    paste0(n, ".", rnames)  # multi-layer raster: base name + original band names
+  }
+}, raster.list, clean.names, SIMPLIFY = TRUE) %>% unlist()
+
+layer.names
+
+swe.stack <- rast(raster.list)
+names(swe.stack) <- layer.names
+
+# convert to dataframe
+swe.df <- as.data.frame(swe.stack, xy = T, na.rm = T)
+
+head(swe.df)
+
