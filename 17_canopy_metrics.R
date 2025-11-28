ddb2b44524a69c0e6b6ac0c843f2933f8fc78fcf
Mosher <km220416@umconnect.umt.edu>
2025-11-28 09:52:10 -0700

    11/26

--- 17_canopy_metrics.R
+++ 17_canopy_metrics.R
@@ -1,5 +1,5 @@
-packages <- c('here', 'terra')
-install.packages(setdiff(packages, row.names(installed.packages())))
+packages <- c('terra', 'sf', 'mapview', 'lidR', 'aRchi', 'TreeLS', 'dplyr')
+install.packages(setdiff(packages, rownames(installed.packages())))
 lapply(packages, library, character.only = T)
 
 
@@ -9,3 +9,13 @@ rm(meancsm)
 summary(mean.csm)
 
 #test
+
+# --- 0) Read catalog & set options ---
+ctg <- readLAScatalog('data/raw/ALS/laz/random_tiles')   # Load a LAScatalog from the folder of tiles
+set_lidr_threads(8)         # Use 8 threads for lidR operations (parallel where supported)
+opt_laz_compression(ctg) <- TRUE      # Write LAZ (compressed) outputs instead of LAS
+opt_progress(ctg)        <- TRUE      # Show a progress bar for catalog operations
+
+plot(ctg, mapview = TRUE, map.types = "Esri.WorldImagery")  # Interactive map of catalog tiles with Esri imagery basemap
+
+tile.dtm = rasterize_terrain(ctg, res = 3.28, algorithm = tin())
