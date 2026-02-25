e4cbda065159f04abf0aed88e162b14abdccc00b
Mosher <km220416@umconnect.umt.edu>
2026-02-25 15:38:50 -0700

    some ASO (2020-2022) had a different origin and and a resolution of 50.00001 instead of 50. I reprojected them to the correct template.

--- 07_snow_data_prep.R
+++ 07_snow_data_prep.R
@@ -9,48 +9,7 @@ swe <- rast(here('data', 'processed', 'processed', 'tif', 'ASO_SanJoaquin_2020_0
 dem <- rast(here('data', 'processed', 'processed', 'tif', 'nasadem_creek_elev.tif'))
 res(dem)
 
-# # 3 methods of resampling from 50m to 30m
-# swe.30.bl <- resample(swe, dem, method = 'bilinear')
-# swe.30.nn <- resample(swe, dem, method = 'near')
-# swe.30.rms <- resample(swe, dem, method = 'rms')
-# 
-# zoom.ext <- c(310000, 311500, 4120000, 4121500)
-# 
-# par(mfrow = c(1,3)) 
-# plot(swe.30.nn, xlim=zoom.ext[1:2], ylim=zoom.ext[3:4], main = "nearest neighbor")
-# plot(swe.30.rms, xlim=zoom.ext[1:2], ylim=zoom.ext[3:4], main = "rms")
-# plot(swe.30.bl, xlim=zoom.ext[1:2], ylim=zoom.ext[3:4], main = "bilinear")
-# 
-# # method of going to 10 first then 30m
-# template.10m <- rast(ext(swe.50m), res=10, crs=crs(swe.50m))
-# swe.10.bl <- resample(swe, template.10m, method = 'bilinear')
-# swe.10.30.bl <- resample(swe.10.bl, dem, method = 'bilinear')
-# plot(swe.10.30.bl, xlim=zoom.ext[1:2], ylim=zoom.ext[3:4], main = "50m to 10m to 30m, bilinear")
-# plot(swe.30.bl, xlim=zoom.ext[1:2], ylim=zoom.ext[3:4], main = "50m to 30m, bilinear")
 
-
-# Don't need this code anymore because we're not resampling SWE
-# #### picking bl for now just as a placeholder to build rest of code
-# in.dir <- here('data', 'processed', 'processed', 'tif')
-# out.dir <- here('data', 'processed', 'processed', 'tif')
-# 
-# # create a list of all swe files to be resampled
-# swe.files <- list.files(in.dir,
-#                         pattern = '^ASO_SanJoaquin_2020.*\\.tif$', # get rid of 2020 when ready to run for all
-#                         full.names = T)
-# 
-# # loop though swe files and resample each, resaving the new result
-# for (f in swe.files) {
-#   r = rast(f)
-#   r.30m <- resample(r, dem, method = 'near') # change method of necessary
-#   
-#   new.name <- basename(f)
-#   new.name <- gsub('50m', '30m', new.name) # replace 50m with 30m 
-#   new.name <- gsub('_clipped', '', new.name) # get rid of "clipped"
-#   
-#   out.name <- file.path(out.dir, new.name)
-#   writeRaster(r.30m, out.name, overwrite = T)
-# }
 
 #### SDD ####
 par(mfrow = c(1,1))
@@ -91,6 +50,7 @@ sdd.creek.5000 <- mask(sdd.creek, dem.5000)
 
 ##### rename SWE files that are clipped to 5000 to reflect that so I can change them to the correct elevations withough losing them
 # List all files in out.dir that start with "ASO_"
+out.dir <- 'J:/Fire_Snow/fireandice/data/processed/processed/tif/50m/creek'
 aso.files <- list.files(out.dir, pattern = '^ASO_', full.names = TRUE)
 
 # Create new names with "_5000" before the extension
@@ -143,5 +103,94 @@ plot(test)
 # crop all to the ref file
 
 
+# ----- check all origins/res/crs -----
+
+out.dir <- 'J:/Fire_Snow/fireandice/data/processed/processed/tif/50m/creek'
+aso.files <- list.files(out.dir, pattern = '^ASO_', full.names = TRUE)
+
+# check CRS
+for (f in aso.files) {
+  r <- rast(f)
+  print(crs(r, describe = T)$code)
+}
+
+# check res
+for (f in aso.files) {
+  r <- rast(f)
+  print(res(r))
+}
+
+# check origin
+for (f in aso.files) {
+  r <- rast(f)
+  print(origin(r))
+}
+
+# since some have different origins and (veryyyy slightly) different resolutions, must match to template grid
+
+template <- rast(aso.files[12]) # has 50 res and 0,0 origin
+
+# put aligned files here temporarily 
+tmp.dir <- file.path(out.dir, 'aso_aligned_temp')
+dir.create(tmp.dir, showWarnings = F)
+
+for (f in aso.files[1:11]) {
+  r <- rast(f)
+  r.align <- project(r, template, method = 'near') # align to template grid
+  writeRaster(r.align, file.path(tmp.dir, basename(f)), overwrite = TRUE)
+}
+
+aligned.aso <- list.files(tmp.dir, pattern = 'ASO', full.names = TRUE)
+
+# verify to make sure they have the correct res, origin, and crs after reprojecting
+
+for (f in aligned.aso) {
+  r <- rast(f)
+  print(res(r))
+}
+
+for (f in aligned.aso) {
+  r <- rast(f)
+  print(origin(r))
+}
+
+
+pre.align <- rast(file.path(out.dir, 'ASO_SanJoaquin_2020_0414_swe_50m_1524.tif'))
+post.align <- rast(file.path(tmp.dir, 'ASO_SanJoaquin_2020_0414_swe_50m_1524.tif'))
+
+plot(pre.align)
+plot(post.align)
+
+names(pre.align)
+names(post.align)
+
+summary(values(pre.align))
+summary(values(post.align))
+
+# write rasters to replace 
+file.copy(list.files(tmp.dir, full.names=TRUE),
+          dirname(aso.files[1]),
+          overwrite=TRUE)
+
+# final check
+aso.files <- list.files(out.dir, pattern = '^ASO_', full.names = TRUE)
+
+# check CRS
+for (f in aso.files) {
+  r <- rast(f)
+  print(crs(r, describe = T)$code)
+}
+
+# check res
+for (f in aso.files) {
+  r <- rast(f)
+  print(res(r))
+}
+
+# check origin
+for (f in aso.files) {
+  r <- rast(f)
+  print(origin(r))
+}
 
 
