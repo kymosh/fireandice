12f6e1a489062bcdc4e17655a0fd23f119ac5787
kymosh <kyliecmosher@gmail.com>
2026-05-05 14:31:16 -0600

    committing changes after I've created the function for my study areas but before I delete the original code

--- 05_clip_study_areas.R
+++ 05_clip_study_areas.R
@@ -1,70 +1,413 @@
-####### this was all test code to make sure clipping the tifs would work before clipping them all together in a for loop
+
+# this code clips study areas to the overlapping extents of the SWE data, and then apply an elevation filter to limit to areas above 5000ft (1524m) where snow is more likely to be present. This will be the final extent for the analysis, and will be used to create a mask for clipping all rasters to the same extent and resolution.
 
 # Load packages
-# packages <- c( 'here', 'exactextractr', 'raster', 'sf', 'terra', 'geodata',
-#   'tidyverse', 'spatialEco', 'patchwork', 'knitr', 'dplyr')
-# install.packages(setdiff(packages, rownames(installed.packages())))
-# lapply(packages, library, character.only = TRUE)
-# 
-# # Study extents
-# castle.extent <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_castle_32611.shp'))
-# creek.extent  <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_creek_32611.shp'))
-# 
-# # Visualize extents
-# plot(st_geometry(creek.extent), col = adjustcolor('green', alpha.f = 0.5), main = 'Creek Study Extent')
-# plot(st_geometry(castle.extent), col = adjustcolor('purple', alpha.f = 0.5), main = 'Castle Study Extent')
-# 
-# Load ASO SWE raster
-# tif <- rast(here('data', 'raw', 'ASO', 'tif', 'ASO_SanJoaquin_2023Jan21-24_swe_50m.tif'))
-# 
-# # Check coordinate reference systems
-# st_crs(creek.extent) == st_crs(tif)
-# 
-# # Clip tif to study extent
-# tif.crop <- crop(tif, vect(creek.extent))    # clip to bounding box
-# tif.mask <- mask(tif.crop, vect(creek.extent))  # mask to actual shape
-
-######################
-# Load packages
-packages <- c( 'here', 'exactextractr', 'raster', 'sf', 'terra', 'geodata',
-  'tidyverse', 'spatialEco', 'patchwork', 'knitr', 'dplyr', 'stringr', 'tools')
+packages <- c('tidyverse', 'sf', 'terra')
 install.packages(setdiff(packages, rownames(installed.packages())))
 lapply(packages, library, character.only = TRUE)
 
+# --- HUC study extent ---
+# read in shape files of study areas
+shp.dir <- 'data/processed/processed/shp/studyarea_extents'
+shp.files <- list.files(shp.dir, pattern = '^study_extent_.*huc\\.shp$', full.names = TRUE)
+hucs.areas <- lapply(shp.files, vect)
 
-# Read study extents
-castle.extent <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_castle_32611.shp'))
-creek.extent  <- st_read(here('data', 'processed', 'processed', 'shp', 'study_extent_creek_32611.shp'))
+# apply names to keep straight which is which
+names(hucs.areas) <- gsub(
+  'study_extent_|_3261.*_huc\\.shp',
+  '',
+  basename(shp.files)
+)
+
+# check
+names(hucs.areas)
+# check crs
+crs(hucs.areas[[1]], describe = TRUE)$code
+
+# --- NASADEMS ---
+# read in NASADEMs (downloaded from GEE)
+dem.files <- list.files('data/raw/background_variables/tif', pattern = '^nasadem.*\\.tif$', full.names = TRUE)
 
 # Read DEMs
-dem.castle <- rast(here('data', 'processed', 'processed', 'tif', 'dem_castle_32611.tif'))
-dem.creek <- rast(here('data', 'processed', 'processed', 'tif', 'dem_creek_32611.tif'))
+dems <- lapply(dem.files, rast)
+# rename
+names(dems) <- gsub(
+  'nasadem_|\\.tif',
+  '',
+  basename(dem.files)
+)
+# check
+names(dems)
+crs(dems[[1]], describe = TRUE)$code
 
-# Load sample ASO SWE raster
-tif <- rast(here('data', 'processed', 'processed', 'tif', 'ASO_Kaweah_2021_0504_swe_50m_clipped.tif'))
+# --- SWE data extents ---
+# combine CASTLE and CALDOR extents to single mosaic because they have two separate watershed extents
+# castle 
+castle.kern <- rast('data/raw/ASO/tif/ASO_Kern_2023Apr29-30_swe_50m.tif')
+castle.kaweah <- rast('data/raw/ASO/tif/ASO_Kaweah_2025Feb21_swe_50m.tif')
+castle.swe <- mosaic(castle.kern, castle.kaweah)
 
-# make sure tifs match extents and dem CRS
-crs(dem.castle, describe = T)$code == crs(tif, describe = T)$code
-crs(tif, describe = T)$code == crs(castle.extent, describe = T)$code
-crs(dem.creek, describe = T)$code == crs(tif, describe = T)$code
-crs(tif, describe = T)$code == crs(creek.extent, describe = T)$code
+# caldor
+caldor.amer <- rast('data/raw/ASO/tif/ASO_American_2023Jun02_swe_50m.tif')
+caldor.truck <- rast('data/raw/ASO/tif/ASO_Truckee_2024May17-18_swe_50m.tif')
+caldor.swe <- mosaic(caldor.amer, caldor.truck)
+
+dixie.swe <- rast('data/raw/ASO/tif/ASO_Feather_2024May13-15_swe_50m.tif')
+creek.swe <- rast('data/raw/ASO/tif/ASO_SanJoaquin_2024Feb22-25_swe_50m.tif')
+
+swe.list <- list(
+  caldor = caldor.swe,
+  castle = castle.swe,
+  creek = creek.swe,
+  dixie = dixie.swe
+)
+
+# ----------------------------------------------------------------------------------
+# Process Study Area function 
+# ----------------------------------------------------------------------------------
+
+calc.study.area <- function(fire, swe.area, hucs.areas, dem, out.dir.shp, out.dir.tif, elev.cutoff = 1524) {
+  
+  message('Processing ', fire, '...')
+  
+  # --- create polygon from swe data footprint ---
+  r.data <- !is.na(swe.area)
+  
+  r.poly <- r.data %>%
+    as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+    subset(.[[1]] == 1) %>%
+    fillHoles() %>%
+    terra::aggregate()
+  
+  # and if statement to make sure CRS code matches
+  
+  # force CRS to match study area (they already should match)
+  crs(r.poly) <- crs(hucs.areas[[fire]])
+  
+  # create polygon only where hucs.area and swe.area overlap
+  overlap <- terra::intersect(hucs.areas[[fire]], r.poly)
+  overlap.parts <- disagg(overlap)
+  overlap.parts$area <- expanse(overlap.parts, unit = 'm')
+  overlap.simple <- overlap.parts[which.max(overlap.parts$area), ]
+  
+  # save
+  # this is the shp file for the study area extent, before being clipped to elevation
+  writeVector(overlap.simple,
+              file.path(out.dir.shp, paste0('study_extent_', fire, '_simple.shp')), 
+              overwrite = TRUE)
+  
+  # --- crop DEM to new study area ---
+  # crop to polygon that we just made
+  dem.crop <- dem %>%
+    crop(overlap.simple) %>%
+    mask(overlap.simple)
+
+  writeRaster(dem.crop,  file.path(out.dir.tif, paste0('dem_', fire, '.tif')), 
+              overwrite = TRUE)
+  
+  # --- crop to only high elevations (>5000ft/1524m)
+  # create mask to limit elevations to >5000ft (1524m)
+  dem.high <- terra::mask(
+    dem.crop,
+    dem.crop > elev.cutoff,
+    maskvalues = FALSE
+  )
+  
+  writeRaster(dem.high,  file.path(out.dir.tif, paste0('dem_', fire, '_', elev.cutoff, '.tif')), 
+              overwrite = TRUE)
+  
+  r.high <- !is.na(dem.high)
+  
+  high.poly <- r.high %>%
+    terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+    subset(.[[1]] == 1) %>%
+    terra::fillHoles() %>%
+    terra::aggregate()
+  
+  writeVector(high.poly, file.path(out.dir.shp, paste0('study_extent_', fire, '_', elev.cutoff, '.shp')), 
+              overwrite = TRUE)
+  
+  return(list(
+    overlap = overlap.simple,
+    dem.crop = dem.crop,
+    dem.high = dem.high,
+    high.poly = high.poly
+  ))
+}
+
+fires <- names(hucs.areas)
+
+out.dir.shp <- 'data/processed/processed/shp/studyarea_extents'
+out.dir.tif <- 'data/processed/processed/tif/30m'
 
 
-######################
+results <- lapply(fires, function(fire) {
+  
+  calc.study.area(
+    fire = fire,
+    swe.area = swe.list[[fire]],
+    hucs.areas = hucs.areas,
+    dem = dems[[fire]],
+    out.dir.shp = out.dir.shp,
+    out.dir.tif = out.dir.tif,
+    elev.cutoff = 1524
+  )
+})
+
+# ----- check results -----
+par(mfrow = c(2, 2))
+
+# simple overlap polygons
+lapply(results, function(x) {
+  plot(x$overlap)
+})
+
+# high-elevation polygons
+lapply(results, function(x) {
+  plot(x$high.poly)
+})
+
+# cropped DEM
+lapply(results, function(x) {
+  plot(x$dem.crop)
+})
+
+# high-elev DEM
+lapply(results, function(x) {
+  plot(x$dem.high)
+})
+
+# check CRS
+crs(castle.swe, describe = T)$code
+
+# --- create shp file of only overlapping extents ---
+
+# visualize
+plot(castle.swe)
+plot(study.areas[[2]], add = TRUE, border = 'red', col = NA)
+
+r.data <- !is.na(castle.swe)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+plot(castle.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas[[2]], add = TRUE, border = 'white', col = NA)
+
+# they are functionally the same already
+terra::crs(r.poly) <- terra::crs(study.areas$castle)
+
+overlap <- terra::intersect(study.areas$castle, r.poly)
+plot(overlap)
+overlap.parts <- disagg(overlap)
+overlap.parts$area <- terra::expanse(overlap.parts, unit = 'm')
+overlap.simple <- overlap.parts[which.max(overlap.parts$area), ]
+
+# visualize
+plot(castle.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas[[2]], add = TRUE, border = 'white', col = NA)
+plot(overlap.simple, , add = TRUE, col = NA, border = 'red', lwd = 2)
+
+# save
+writeVector(overlap.simple, 'data/processed/processed/shp/study_extent_castle_32611_simple.shp', overwrite = TRUE)
+
+# --- Elevation filtering ---
+# crop to polygon that we just made
+dem.crop <- dems$castle %>%
+  terra::crop(overlap.simple) %>%
+  terra::mask(overlap.simple)
+
+plot(dem.crop)
+writeRaster(dem.crop, 'data/processed/processed/tif/30m/dem_castle.tif', overwrite = TRUE)
 
 # create mask to limit elevations to >5000ft (1524m)
-creek.mask <- dem.creek > 1524
-castle.mask <- dem.castle > 1524
+dem.high <- terra::mask(
+  dem..crop,
+  dem.crop > 1524,
+  maskvalues = FALSE
+)
 
-# use mask to filter DEM
-dem.creek.5000 <- mask(dem.creek, creek.mask, maskvalue = 0)
-dem.castle.5000 <- mask(dem.castle, castle.mask, maskvalue = 0)
-# plot(dem.castle.5000)
-# plot(dem.creek.5000)
+plot(dem.high)
+writeRaster(dem.high, 'data/processed/processed/tif/30m/dem_castle_1524.tif', overwrite = TRUE)
+
+r.data <- !is.na(dem.high)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+writeVector(r.poly, 'data/processed/processed/shp/study_extent_castle_32611_1524.shp', overwrite = TRUE)
+
+# ----------------------------------------------------------------------------------
+# CALDOR Fire
+# ----------------------------------------------------------------------------------
+
+
+# check CRS
+crs(caldor.swe, describe = T)$code
+
+# ----- create shp file of only overlapping extents 
+# visualize
+plot(caldor.swe)
+plot(study.areas$caldor, add = TRUE, border = 'red', col = NA)
+
+r.data <- !is.na(caldor.swe)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+plot(caldor.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas$caldor, add = TRUE, border = 'white', col = NA)
+
+crs(r.poly, describe = T)$code 
+crs(study.areas$caldor, describe = T)$code 
+
+# they are functionally the same already
+terra::crs(r.poly) <- terra::crs(study.areas$caldor)
+
+overlap <- terra::intersect(study.areas$caldor, r.poly)
+plot(overlap)
+overlap.parts <- disagg(overlap)
+overlap.parts$area <- terra::expanse(overlap.parts, unit = 'm')
+overlap.simple <- overlap.parts[which.max(overlap.parts$area), ]
+
+# visualize
+plot(caldor.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas$caldor, add = TRUE, border = 'white', col = NA)
+plot(overlap.simple, , add = TRUE, col = NA, border = 'red', lwd = 2)
+
+# save
+writeVector(overlap.simple, 'data/processed/processed/shp/study_extent_caldor_32610_simple.shp', overwrite = TRUE)
+
+# ----- Elevation filtering 
+
+# crop to polygon that we just made
+dem.crop <- dems$caldor %>%
+  terra::crop(overlap.simple) %>%
+  terra::mask(overlap.simple)
+
+plot(dem.crop)
+writeRaster(dem.crop, 'data/processed/processed/tif/30m/dem_caldor.tif', overwrite = TRUE)
+
+# create mask to limit elevations to >5000ft (1524m)
+dem.high <- terra::mask(
+  dem.crop,
+  dem.crop > 1524,
+  maskvalues = FALSE
+)
+
+plot(dem.high)
+writeRaster(dem.high, 'data/processed/processed/tif/30m/dem_caldor_1524.tif', overwrite = TRUE)
+
+r.data <- !is.na(dem.high)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+plot(r.poly)
+
+writeVector(r.poly, 'data/processed/processed/shp/study_extent_caldor_32610_1524.shp', overwrite = TRUE)
 
 
 
 
+
+
+
+# ----------------------------------------------------------------------------------
+# DIXIE Fire
+# ----------------------------------------------------------------------------------
+
+# combine
+dixie.swe <- rast('data/raw/ASO/tif/ASO_Feather_2025Mar24-25_swe_50m.tif')
+
+# check CRS
+crs(caldor.swe, describe = T)$code
+
+# ----- create shp file of only overlapping extents 
+# visualize
+plot(caldor.swe)
+plot(study.areas$caldor, add = TRUE, border = 'red', col = NA)
+
+r.data <- !is.na(caldor.swe)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+plot(caldor.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas$caldor, add = TRUE, border = 'white', col = NA)
+
+crs(r.poly, describe = T)$code 
+crs(study.areas$caldor, describe = T)$code 
+
+# they are functionally the same already
+terra::crs(r.poly) <- terra::crs(study.areas$caldor)
+
+overlap <- terra::intersect(study.areas$caldor, r.poly)
+plot(overlap)
+overlap.parts <- disagg(overlap)
+overlap.parts$area <- terra::expanse(overlap.parts, unit = 'm')
+overlap.simple <- overlap.parts[which.max(overlap.parts$area), ]
+
+# visualize
+plot(caldor.swe)
+plot(r.poly, add = T, border = 'red', col = NA)
+plot(study.areas$caldor, add = TRUE, border = 'white', col = NA)
+plot(overlap.simple, , add = TRUE, col = NA, border = 'red', lwd = 2)
+
+# save
+writeVector(overlap.simple, 'data/processed/processed/shp/study_extent_caldor_32610_simple.shp', overwrite = TRUE)
+
+# ----- Elevation filtering 
+
+# crop to polygon that we just made
+dem.crop <- dems$caldor %>%
+  terra::crop(overlap.simple) %>%
+  terra::mask(overlap.simple)
+
+plot(dem.crop)
+writeRaster(dem.crop, 'data/processed/processed/tif/30m/dem_caldor.tif', overwrite = TRUE)
+
+# create mask to limit elevations to >5000ft (1524m)
+dem.high <- terra::mask(
+  dem.crop,
+  dem.crop > 1524,
+  maskvalues = FALSE
+)
+
+plot(dem.high)
+writeRaster(dem.high, 'data/processed/processed/tif/30m/dem_caldor_1524.tif', overwrite = TRUE)
+
+r.data <- !is.na(dem.high)
+
+r.poly <- r.data %>%
+  terra::as.polygons(aggregate = TRUE, dissolve = TRUE) %>%
+  subset(.[[1]] == 1) %>%
+  terra::fillHoles() %>%
+  terra::aggregate()
+
+plot(r.poly)
+
+writeVector(r.poly, 'data/processed/processed/shp/study_extent_caldor_32610_1524.shp', overwrite = TRUE)
 
 
 
@@ -73,7 +416,7 @@ dem.castle.5000 <- mask(dem.castle, castle.mask, maskvalue = 0)
 # ASO tifs
 
 # Directory with input ASO tifs
-tif.dir <- here('data', 'raw', 'ASO', 'tif')
+tif.dir <- 'data/raw/ASO/tif'
 
 # Output directory
 out.dir.tif <- here('data', 'processed', 'processed', 'tif')
